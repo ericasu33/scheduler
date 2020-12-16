e1c770531f370b01f2ad9f813057b4ffde48a9ce
Erica <ericasu33@gmail.com>
2020-12-15 20:08:58 -0800

    fix back error on the earlier testing for circlecli

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,5 +1,5 @@
 
-// export const SET_DAY = "SET_DAY";
+export const SET_DAY = "SET_DAY";
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_INTERVIEW = "SET_INTERVIEW";
 
